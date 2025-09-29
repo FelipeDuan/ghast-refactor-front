255f9dc0f23a23a6c58e637f3477a56d6122ea06
FelipeDuan <felipeduan1@gmail.com>
2025-09-29 13:32:12 -0300

    feat: ajuste rotas

--- src/app/(auth)/(login)/page.tsx
+++ src/app/(auth)/(login)/page.tsx
@@ -1,6 +1,6 @@
 import { GalleryVerticalEnd } from 'lucide-react';
 import Link from 'next/link';
-import { LoginForm } from '@/app/(auth)/login/_components/login-form';
+import { LoginForm } from '@/app/(auth)/(login)/_components/login-form';
 
 export default function LoginPage() {
   return (
