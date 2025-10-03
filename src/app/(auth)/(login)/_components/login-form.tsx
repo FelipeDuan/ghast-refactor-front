7a2bda6c75fbfeecf65c029e31d74b43b57bdfd9
FelipeDuan <felipeduan1@gmail.com>
2025-10-02 22:58:15 -0300

    feat: leve refact na estrutura da page

--- src/app/(auth)/(login)/_components/login-form.tsx
+++ src/app/(auth)/(login)/_components/login-form.tsx
@@ -1,8 +1,4 @@
-'use client';
-
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import z from 'zod';
+import { SignInForm } from '@/components/auth/sign-in-form';
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -11,41 +7,13 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
 import { SocialLogin } from './social-login';
-
-const formSchema = z.object({
-  email: z.email('Email inválido.'),
-  password: z.string('Senha inválida').min(6, 'Mínimo de 6 caracteres'),
-});
-
-type FormValues = z.infer<typeof formSchema>;
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<'div'>) {
-  const form = useForm<FormValues>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-  });
-
-  function onSubmit(values: FormValues) {
-    console.log(values);
-  }
-
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
       <Card>
@@ -60,62 +28,14 @@ export function LoginForm({
             <div className="flex flex-col gap-4">
               <SocialLogin />
             </div>
+
             <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
               <span className="bg-card text-muted-foreground relative z-10 px-2">
                 Ou continue com
               </span>
             </div>
 
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="grid gap-6"
-              >
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => {
-                    return (
-                      <FormItem>
-                        <FormLabel>Email</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="email"
-                            placeholder="email@exemplo.com"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    );
-                  }}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => {
-                    return (
-                      <FormItem>
-                        <FormLabel>Senha</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="password"
-                            placeholder="Digite sua senha"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    );
-                  }}
-                />
-
-                <Button type="submit" className="w-full">
-                  Login
-                </Button>
-              </form>
-            </Form>
+            <SignInForm />
 
             <div className="text-center text-sm">
               Ainda não tem uma conta?
---
+++ src/components/auth/sign-in-form.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const formSchema = z.object({
+  email: z.email('Email inválido.'),
+  password: z.string('Senha inválida').min(6, 'Mínimo de 6 caracteres'),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+export function SignInForm() {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(values: FormValues) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-6">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="email@exemplo.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Digite sua senha"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+      </form>
+    </Form>
+  );
+}
