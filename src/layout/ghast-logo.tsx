2a4492d63c2c74f7a66d1009c304cd9a941ab597
FelipeDuan <felipeduan1@gmail.com>
2025-10-08 01:10:21 -0300

    feat: add shimmering text

--- src/layout/ghast-logo.tsx
+++ src/layout/ghast-logo.tsx
@@ -1,12 +1,20 @@
 import { GalleryVerticalEnd } from 'lucide-react';
+import { ShimmeringText } from '@/components/ui/shimmering-text';
 
 export function GhastLogo() {
   return (
     <div className="text-foreground text flex items-center gap-2 self-center py-2 text-lg font-semibold">
       <GalleryVerticalEnd className="size-5" />
-      {/* <div className="bg-primary flex items-center justify-center rounded-md p-1.5 text-white">
-      </div> */}
-      Ghast Consultancy
+
+      <ShimmeringText
+        text="Ghast Consultancy"
+        color="var(--color-muted-foreground)"
+        shimmerColor="var(--color-foreground)"
+        duration={1.5}
+        repeatDelay={1}
+        once
+        repeat={false}
+      />
     </div>
   );
 }
